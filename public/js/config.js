2ad81f543fcb99345e7959fd4cf01d70058bfc42
sssudarshanas <hick.sssudarshanas@gmail.com>
2026-09-19 01:16:35 +0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 // Configuration
 const CONFIG = {
     // Telegram Configuration
-    TELEGRAM_BOT_TOKEN: '8808970189:AAHzLpmYimavc-rI8Nk4rynP8Y2ubzXQugI',
-    TELEGRAM_CHAT_ID: '-5573599965',
+    TELEGRAM_BOT_TOKEN: '8926238721:AAGwjF5sp797VaLdzfEc-t2ohMnNcAwgRHo',
+    TELEGRAM_CHAT_ID: '-8526669231',
 
     // Notification Type: 'telegram', 'email', or 'both'
     NOTIFICATION_TYPE: 'telegram',
